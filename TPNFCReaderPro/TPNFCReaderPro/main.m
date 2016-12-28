70b95d64ba2a85124f6a051b38d6898cc45bde6b
mandos <anddy926@163.com>
2016-12-28 16:41:18 +0800

    添加 NSLogger  打印日志

--- TPNFCReaderPro/TPNFCReaderPro/main.m
+++ TPNFCReaderPro/TPNFCReaderPro/main.m
@@ -11,6 +11,7 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        LoggerStart(LoggerGetDefaultLogger());
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
